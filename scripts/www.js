443817582093cba03816c9ef2d652c48a1800df6
mnlcandelaria <mnl.candelaria@gmail.com>
2019-02-25 00:29:02 +0800

    8eves complete

--- scripts/www.js
+++ scripts/www.js
@@ -39,4 +39,113 @@ jQuery(function($) {
     // random banner
     var banner = Math.floor(Math.random() * 5) + 1;
     $('.banner').addClass('banner-' + banner);
+
+    // call fuction for form inquiry
+    formContact();
 });
+
+function formContact() {
+    $('#btnSubmit').click(function() {
+        var hasError = false;
+
+        $('form[name="form-contact"] input').removeClass('is-invalid');
+        $('form[name="form-contact"] textarea').removeClass('is-invalid');
+        $('form[name="form-contact"]').find('.alert').remove();
+
+        if (!$('#inputFullName').val() || $('#inputFullName').val().trim() === '') {
+            $('#inputFullName')
+                .addClass('is-invalid')
+                .val('')
+                .parent()
+                .find('.invalid-feedback')
+                .text('Full Name is required.');
+
+            hasError = true;
+        }
+
+        if (!$('#inputEmail').val() || $('#inputEmail').val().trim() === '') {
+            $('#inputEmail')
+                .addClass('is-invalid')
+                .val('')
+                .parent()
+                .find('.invalid-feedback')
+                .text('Email is required.');
+
+            hasError = true;
+        } else if (!validateEmail($('#inputEmail').val())) {
+            $('#inputEmail')
+                .addClass('is-invalid')
+                .parent()
+                .find('.invalid-feedback')
+                .text('Invalid Email Address.');
+
+            hasError = true;
+        }
+
+        if (!$('#inputContact').val() || $('#inputContact').val().trim() === '') {
+            $('#inputContact')
+                .addClass('is-invalid')
+                .val('')
+                .parent()
+                .find('.invalid-feedback')
+                .text('Contact Number is required.');
+
+            hasError = true;
+        }
+
+        if (!$('#textareaMessage').val() || $('#textareaMessage').val().trim() === '') {
+            $('#textareaMessage')
+                .addClass('is-invalid')
+                .val('')
+                .parent()
+                .find('.invalid-feedback')
+                .text('Message is required.');
+
+            hasError = true;
+        }
+
+        if (!hasError) {
+            // submit form
+            $('form[name="form-contact"]').submit();
+        } else {
+            // add message error template
+            $('form[name="form-contact"]').prepend(
+                '<div class="alert alert-danger">' +
+                'Please fill-in the required details.' +
+                '</div>'
+            );
+        }
+    });
+
+    $('form[name="form-contact"]').on('submit', function(e) {
+        e.preventDefault();
+
+        // submit response
+        $.ajax({
+            url: $('form[name="form-contact"]').attr('action'),
+            type: 'POST',
+            data: $('form[name="form-contact"]').serialize()
+        });
+
+        // set form fields to readonly
+        $('form[name="form-contact"] input').attr('readonly', true);
+        $('form[name="form-contact"] textarea').attr('readonly', true);
+        $('form[name="form-contact"] button#btnSubmit')
+            .removeClass('btn-primary')
+            .addClass('btn-secondary')
+            .attr('disabled', 'disabled');
+
+        // add message success template
+        $('form[name="form-contact"]').prepend(
+            '<div class="alert alert-success">' +
+            'Your response had been submitted successfully!' +
+            '</div>'
+        );
+    });
+
+    // validate email address
+    function validateEmail(email) {
+        var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return regex.test(String(email).toLowerCase());
+    }
+}
